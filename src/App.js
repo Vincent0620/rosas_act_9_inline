0114d7bd37f274b59a4f213046c7d61d90bcef4e
Vincent0620 <marcusvincentrosas@thelewiscollege.edu.ph>
2023-12-06 21:11:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+function InlineStyling() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: "auto",}}>
+      <div style={{width: "750px", margin: "auto", fontFamily: "Comic Sans MS"}}>
+        <div style={{textAlign: "Center",}}>
+          <h3>Inline</h3>
+        </div>
+        <div style={{background: "orange", padding: "6% 0 0",height: "850px", width: "750px",}}>
+          <div style={{height: "10%", textAlign: "Center",}}>
+            <h3>Login Form</h3>
+          </div>
+          <div>
+            <div style={{margin: "10%"}}>
+              <form>
+                  <input style={{boxSizing: "border-box",width: "100%", marginBottom: "20px", fontSize: "14px", padding: "15px",}} type="email" placeholder="Email Address" />
+                  <input style={{boxSizing: "border-box",width: "100%",marginBottom: "20px",fontSize: "14px", padding: "15px",}} type="password" placeholder="Password" />
+                  <button style={{width: "100%", padding: "10px" ,backgroundColor: "Green", color: "white", cursor: "pointer"}}>LOGIN</button>
+              </form>
+              <div style={{marginTop: "15px", marginBottom: "15px", display: "flex", alignItems: "Center", justifyContent: "Center",}}>
+                <div style={{width: "100%"}}>
+                  <hr></hr>
+                </div>
+                <div>
+                  <p style={{opacity: "0.6"}}>OR</p>
+                </div>
+                <div style={{width: "100%"}}>
+                  <hr></hr>
+                </div>
+              </div>
+              <div style={{display: "flex", alignItems: "Center", justifyContent: "space-evenly",}}>
+                <a href="#"><img style={{width: "100px", height: "100px", cursor: "pointer",}} src={logo}></img></a>
+                
+              </div>
+              <div style={{textAlign: "Center", margin: "45px", opacity: "0.8"}}>
+                <p>No Account? <a href = "#">Create Account Here!</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default InlineStyling;
+
+
+
+
